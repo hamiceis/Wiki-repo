be686b3e67736c0971876c3867c0189030a6a0a1
hamiceis <hamiceis@hotmail.com>
2022-10-28 15:32:04 -0300

    Desafio de criar um Wiki de repositorios

---
+++ src/components/Input/index.jsx
@@ -0,0 +1,26 @@
+import { forwardRef } from 'react'
+import Button from '../Button'
+import { InputContainer } from './styled'
+
+export const Input = forwardRef(({dataRepos ,...props}, ref) => {
+  return (
+    <InputContainer>
+      <input ref={ref} {...props} />
+      <Button onClick={dataRepos}/>
+    </InputContainer>
+  )
+});
+
+
+
+// function Input({dataRepos, ...props}, ref) {
+//   return (
+//     <InputContainer>
+//       <input {...props} ref={ref}/>
+//       <Button onClick={dataRepos}/>
+//     </InputContainer>
+//   )
+// }
+
+
+
